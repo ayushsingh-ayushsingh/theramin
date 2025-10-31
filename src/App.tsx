b5579ca223008bf9b80e223b5f8953f0d509d141
Ayush Singh <ayushpno@gmail.com>
2025-10-31 18:55:47 +0530

    Setup tone.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,56 @@
-import Test from "@/components/layouts/interactive-continuum";
-// import Test from "@/components/layouts/continuum";
+import React, { useEffect } from "react";
+import * as Tone from "tone";
+import { Button } from "./components/ui/button";
 
 function App() {
+  const synth = new Tone.Synth();
+  const feedbackDelay = new Tone.FeedbackDelay({
+    delayTime: 0.1,
+    feedback: 0.9,
+    maxDelay: 10,
+  });
+
+  useEffect(() => {
+    synth.connect(feedbackDelay);
+    feedbackDelay.toDestination();
+
+    const resumeAudioContext = async () => {
+      if (Tone.context.state === "suspended") {
+        await Tone.context.resume();
+      }
+    };
+
+    window.addEventListener("click", resumeAudioContext);
+
+    return () => {
+      window.removeEventListener("click", resumeAudioContext);
+    };
+  }, [synth, feedbackDelay]);
+
+  const handleKeyDown = async (
+    event: React.KeyboardEvent<HTMLInputElement>
+  ): Promise<void> => {
+    if (event.key === "k") {
+      await Tone.start(); // Ensure this is awaited
+      synth.triggerAttackRelease(440, 1);
+      console.log("K was pressed!");
+    }
+  };
+
   return (
-    <div>
-      <Test />
-      {/* <Continuum fingerX={0} fingerY={0} /> */}
-      {/* <Continuum /> */}
-      {/* <Hand /> */}
+    <div
+      className="w-screen h-screen flex items-center justify-center"
+      tabIndex={0} // To enable key events when focusing on the div
+      onKeyDown={handleKeyDown} // Attach the event listener to the div
+    >
+      <Button
+        onClick={async () => {
+          await Tone.start();
+          synth.triggerAttackRelease(440, 1);
+        }}
+      >
+        Play
+      </Button>
     </div>
   );
 }
